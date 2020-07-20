01224fb2c83ef46f62cdc1f164abad70a87e4914
Lab User <labuser@ENGR300324D.local>
2020-07-20 18:38:02 -0400

    correct timestamp error

--- influxexample/writecheckme.py
+++ influxexample/writecheckme.py
@@ -4,6 +4,8 @@ import math
 import datetime
 import subprocess
 import sys
+import random
+
 
 if len(sys.argv) <= 4:
         print("Example: " + sys.argv[0] + " 3.136.84.223:8086 testdb test sensorweb")
@@ -13,9 +15,9 @@ if len(sys.argv) <= 4:
 
 f_sampling = 10
 f_wave = 1
-timestamp = datetime.datetime.now().timestamp()
 
 while True:
+    timestamp = datetime.datetime.now().timestamp()
     # IP:port, databaseName, user, password
     http_post = "curl -i -XPOST \'http://%s/write?db=%s\' -u %s:%s --data-binary \'" % (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
     for i in range(f_sampling):
@@ -53,7 +55,7 @@ while True:
     http_post = "curl -i -XPOST \'http://%s/write?db=%s\' -u %s:%s --data-binary \'" % (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
     for i in range(f_sampling):
         timestamp += 1 / f_sampling
-        value = math.sin(2 * math.pi * f_wave * timestamp)
+        value = random.randint(35, 43)#math.sin(2 * math.pi * f_wave * timestamp)
         http_post += "\ncheckme,location=E7:CC:E2:D0:5C:C5 temperature="
         http_post += str(value) + " " + str(int(timestamp*10e8))
         time.sleep(1 / f_sampling)
