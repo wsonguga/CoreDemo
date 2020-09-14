6a4fcdaa00d02e7b172195c1adc7ca380340f265
WenZhan Song <wsong@JSong.hsd1.ga.comcast.net>
2020-09-14 09:45:34 -0400

    add more organized influxcopy

--- influxexample/influxcopy.py
+++ influxexample/influxcopy.py
@@ -1,110 +1,145 @@
+#!/usr/bin/env python3
+# Migrate the data from one Influxdb database to another.
+# add argument '-h' for help
+
+# Usage:
+
+# influxcopy.py sURL sDB dURL dDB startTime endTime
+# positional arguments:
+
+# sURL the URL of source database.
+# sDB name of the source database.
+# dURL the URL of destination database.
+# dDB name of the destination database.
+# startTime start time. format: Year-Mon-Day-Hour-Min-Sec
+# endTime end time. format: Year-Mon-Day-Hour-Min-Sec
+
+
+import argparse
 from influxdb import InfluxDBClient
-from influx import InfluxDB
 import numpy as np
-import itertools 
+import time
 import math
 import datetime
-import subprocess
 import sys
-import operator
 from datetime import datetime 
-from datetime import timezone
-import types
-
-#Remote data base information
-IP = 'localhost'
-PORT = 8086
-USER = 'sili'
-PASSWORD = 'sensorweb'
-DBNAME = 'floorseis'
 
 
-#Local database information
-Local_Database_name = 'test2'
-
-#Time range
-starttime = datetime(2020, 6, 4,15,40,0)
-endtime = datetime(2020, 8, 4,15,41,0)
-
-
-
-
-#starttime = "2020-06-04T15:40:00.000000000Z"
-#endtime = "2020-06-04T15:42:00.000000000Z"
-
-
-#utc_time = starttime.replace(tzinfo = timezone.utc)
-timestamp = starttime.timestamp()*1000
-start_str = str(int((timestamp)*1000000))
-
-#utc_time = endtime.replace(tzinfo = timezone.utc)
-timestamp = endtime.timestamp()*1000
-end_str=str(int((timestamp)*1000000))
-
-print(start_str,end_str)
-
-
-client = InfluxDBClient(IP, PORT, USER, PASSWORD, DBNAME)
-client2 = InfluxDB('http://127.0.0.1:8086')
-
-#get series
-
-query = 'show series'
-result = client.query(query)
-
-points = list(result.get_points())
-#value =  np.append(value,np.array(list(map(operator.itemgetter('value'), points))))
-print(points[0]['key'])
-print(points)
-
-def copy_all_points(sname):
-    sname='test'
-    query = 'SELECT * FROM '+ sname +' WHERE time > '+start_str+' and time < '+end_str
-    result = client.query(query)
-    values = list(result.get_points())
-    print(values[0])
-
-    for point in values:
-        tag={}
-        field={}
-        for key, v in point.items():
-            if key=='time':
-                point_time = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%fZ')
-            elif type(v) == type('a'):
-                tag[key]=v
-            else:
-                field[key]=v
-        #print(tag,field,point_time)
-        client2.write(Local_Database_name, sname, fields=field,tags=tag,time=point_time)
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Migrate data from one influxDB database to another.')
+    parser.add_argument('sURL',
+                        type=str,
+                        help='the URL of source database.')
+    parser.add_argument('sDB',
+                        type=str,
+                        help='name of the source database.')
+    parser.add_argument('dURL',
+                        type=str,
+                        help='the URL of destination database.')
+    parser.add_argument('dDB',
+                        type=str,
+                        help='name of the destination database.')
+    parser.add_argument('startTime',
+                        type=str,
+                        help='start time. format: Year-Mon-Day-Hour-Min-Sec')
+    parser.add_argument('endTime',
+                        type=str,
+                        help='end time. format: Year-Mon-Day-Hour-Min-Sec')
+    
+    return parser.parse_args()
 
 
+def datetime_convert(startDate, endDate):
+    year, mon, day, hour, min, sec = startDate.split('-')
+    sDate = datetime(int(year), int(mon), int(day), int(hour), int(min), int(sec))
+
+    year, mon, day, hour, min, sec = endDate.split('-')
+    eDate = datetime(int(year), int(mon), int(day), int(hour), int(min), int(sec))
+
+    return sDate, eDate
 
 
+def data_migration(startTime, endTime, args):
+    
+    timestamp = startTime.timestamp()*1000
+    start_str = str(int((timestamp)*1000000))
 
-#get and upload values
+    timestamp = endTime.timestamp()*1000
+    end_str=str(int((timestamp)*1000000))
 
+    result = sClient.query('show series')
+    points = list(result.get_points())
 
-for i in range(len(points)):
-    read = points[i]['key']
-    sname = read.split(',')[0] 
-    query = 'SELECT * FROM '+ sname +' WHERE time > '+start_str+' and time < '+end_str
-    result = client.query(query)
-    values = list(result.get_points())
-    print(values[0])
+    for i in range(len(points)):
+        read = points[i]['key']
+        sname = read.split(',')[0]
+        copyQuery = 'SELECT * FROM ' + sname + ' WHERE time > ' + start_str + ' and time < ' + end_str
+        result = sClient.query(copyQuery)
+        values = list(result.get_points())
 
-    for point in values:
-        tag={}
-        field={}
-        for key, v in point.items():
-            if key=='time':
-                if len(v)<21:
-                    point_time = datetime.strptime(v, '%Y-%m-%dT%H:%M:%SZ')
+        data = []
+        for point in values:
+            tag = {}
+            field = {}
+            for key, value in point.items():
+                if key == 'time':
+                    if len(value) < 21:
+                        point_time = datetime.strptime(value, '%Y-%m-%dT%H:%M:%SZ')
+                    else:
+                        point_time = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%fZ')
+                elif type(value) == type('a'):
+                    tag[key] = value
                 else:
-                    point_time = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%fZ')
-            elif type(v) == type('a'):
-                tag[key]=v
-            else:
-                field[key]=v
-        #print(tag,field,point_time)
-        client2.write(Local_Database_name, sname, fields=field,tags=tag,time=point_time)
+                    field[key] = value
 
+                data.append(
+                    {
+                        "measurement": sname,
+                        "tags" : tag,
+                        "fields" : field,
+                        "time": point_time
+                    }
+                )
+        dClient.write_points(data, database = args.dDB, time_precision = 'ms', batch_size = write_batch_size, protocol = 'json')
+    return None
+    
+
+def main():
+    sTime, eTime = datetime_convert(args.startTime, args.endTime)
+    leftWindow = sTime
+
+    client_write_start_time = time.perf_counter()
+    while True:
+        if (eTime - leftWindow).seconds > 5 * 60:
+            rightWindow = leftWindow + datetime.timedelta(minutes=5)
+        else:
+            rightWindow = eTime
+
+        data_migration(leftWindow, rightWindow, args)
+        if (eTime - leftWindow).seconds < 5 * 60:
+            break
+        leftWindow = rightWindow
+    
+    client_write_end_time = time.perf_counter()
+    print("Migration completed! Data write time: {time}s".format(time = client_write_end_time - client_write_start_time))
+    return None
+
+
+
+if __name__ == "__main__":
+    args = get_arguments()
+    # The ideal batch size for InfluxDB is 5,000-10,000 points.
+    write_batch_size = 5000
+    sClient = InfluxDBClient(host=args.sURL, 
+                            port=8086, 
+                            username='root', 
+                            password='root', 
+                            database=args.sDB)
+    
+    dClient = InfluxDBClient(host=args.dURL, 
+                            port=8086, 
+                            username='root', 
+                            password='root', 
+                            database=args.dDB,
+                            ssl=True)
+    main()
