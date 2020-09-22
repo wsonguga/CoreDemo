75456e6a85c96c18dafbc30edb75a3977421b798
aaronli-uga <64019008+aaronli-uga@users.noreply.github.com>
2020-09-22 15:47:34 -0400

    Update influxcopy.py

--- influxexample/influxcopy.py
+++ influxexample/influxcopy.py
@@ -4,15 +4,23 @@
 
 # Usage:
 
-# influxcopy.py sURL sDB dURL dDB startTime endTime
+# influxDB_copy.py sURL sDB sUser sPasswd dURL dDB dUser dPasswd startTime endTime
+# For example: 
+# 'python influxDB_copy.py https://sensorweb.us shake test sensorweb https://sensorweb.us testdb test sensorweb 2020-08-07T19:22:31 2020-08-07T19:22:35'
+
 # positional arguments:
 
-# sURL the URL of source database.
-# sDB name of the source database.
-# dURL the URL of destination database.
-# dDB name of the destination database.
-# startTime start time. format: Year-Mon-Day-Hour-Min-Sec
-# endTime end time. format: Year-Mon-Day-Hour-Min-Sec
+#   - sURL        the URL of source database. e.g. "http://localhost"
+#   - sDB         name of the source database.
+#   - sUser       username of the source database.
+#   - sPasswd     password of the source database.
+#   - dURL        the URL of destination database.
+#   - dDB         name of the destination database.
+#   - dUser       username of the destination database.
+#   - dPasswd     password of the destination database.
+#   - startTime   start time. format: Year-Mon-Day-Hour-Min-Sec. e.g. "2020-06-04T15:40:00"
+#   - endTime     end time. format: Year-Mon-Day-Hour-Min-Sec. e.g. "2020-06-05T15:40:00"
+
 
 
 import argparse
