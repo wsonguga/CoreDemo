b54a10cf321dcad08994b33f73a53a75e514688a
WenZhan Song <wsong@uga.edu>
2022-04-30 20:12:28 -0400

    Update shakeClient.py

--- influxexample/shakeClient.py
+++ influxexample/shakeClient.py
@@ -16,7 +16,7 @@ host = "127.0.0.1"
 port = 8888                             # Port to bind to
 sock = s.socket(s.AF_INET, s.SOCK_DGRAM | s.SO_REUSEADDR)
 sock.bind((host, port))
-print "Waiting for data on Port:", port
+# print "Waiting for data on Port:", port
 while 1:                                # loop forever
    data, addr = sock.recvfrom(1024)    # wait to receive data
    print data
