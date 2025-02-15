481713a977b7e511f0df071b0f57a47e9b868650
WenZhan Song <wsong@uga.edu>
2025-02-15 09:02:08 -0500

    Update add_cronjob.sh

--- socketexample/add_cronjob.sh
+++ socketexample/add_cronjob.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# replace /path/to/your/command with your script's actual full path
+# replace /path/to/your/command with cron_socket.sh's actual full path
 (crontab -l 2>/dev/null; echo "* * * * * /path/to/your/command/cron_socket.sh") | crontab -
