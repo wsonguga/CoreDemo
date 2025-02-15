24476de62ca2bfdc0d9f70d3e2d60bd161fc44ef
WenZhan Song <wsong@uga.edu>
2025-02-15 08:52:52 -0500

    Create cron_socket.sh

---
+++ socketexample/cron_socket.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+clear
+PYTHON=$(which python3)
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+#echo "Script path: $SCRIPTPATH"
+now="$(date +'%d/%m/%Y %T')"
+
+res=$(/usr/bin/ntpstat)
+rc=$?
+while [ $rc != 0 ]
+do
+    echo "NTP not running, sleep 1 second at $now"
+    sleep 1
+done
+
+SERVICE="$SCRIPTPATH/socket_to_mqtt.py"
+LOG="log.txt"
+process=$(pgrep -f "$SERVICE")
+process=${process[0]}
+#echo "the process ID of $mac is $process"
+
+if [[ ! -z $process ]]
+then
+    echo "$SERVICE is running at $now"
+else
+#    echo "$SERVICE stopped at $now, restart!" >> $SCRIPTPATH/$LOG
+    cd $SCRIPTPATH
+    sudo $PYTHON $SERVICE
+fi
